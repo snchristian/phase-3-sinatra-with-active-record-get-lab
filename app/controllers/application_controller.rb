2e14557d9ceb6e8b1439dcbb74ed738c250fcaae
Shantel <snchristian8@gmail.com>
2022-03-18 22:12:35 -0400

    commit tree 8c9da1d890c46767aba18de5f7f685fbc448a953

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,23 @@
 class ApplicationController < Sinatra::Base
-
+  set :default_content_type, 'application/json'
   # add routes
+  get '/bakeries' do
+    bakeries = Bakery.all
+    bakeries.to_json
+  end
+
+    get '/bakeries/:id' do
+    bakeries_id = Bakery.find(params[:id])
+    bakeries_id.to_json(include: :baked_goods)
+  end
+    get '/baked_goods/by_price' do
+  bakeries_price = BakedGood.order(price: :desc)
+  bakeries_price.to_json
+end
+
+  get '/baked_goods/most_expensive' do
+expensive_baked_good = BakedGood.order(price: :desc).first
+expensive_baked_good.to_json(only: [:name, :price])
+  end
 
 end
